22755af0366a7533c46873c17807c6f2ed92d252
Dominik Cebula <dominikcebula@gmail.com>
2024-08-08 20:20:14 +0200

    Dynamic Programming Algorithms - Coin Change

--- src/test/java/com/dominikcebula/algorithms/dynamic/programming/CoinChangeTest.java
+++ src/test/java/com/dominikcebula/algorithms/dynamic/programming/CoinChangeTest.java
@@ -17,18 +17,18 @@ class CoinChangeTest {
                 Arguments.of(3, new int[]{5, 7, 8}, 0),
                 Arguments.of(4, new int[]{1, 2, 3}, 4),
                 Arguments.of(5, new int[]{1, 2, 5}, 4),
+                Arguments.of(6, new int[]{1, 3, 4}, 4),
                 Arguments.of(8, new int[]{1, 2, 3}, 10),
-                Arguments.of(6, new int[]{1, 3, 4}, 7),
                 Arguments.of(10, new int[]{2, 5, 3, 6}, 5),
-                Arguments.of(11, new int[]{2, 3, 5, 7}, 4),
+                Arguments.of(11, new int[]{2, 3, 5, 7}, 5),
                 Arguments.of(12, new int[]{1, 2, 5}, 13),
-                Arguments.of(14, new int[]{1, 2, 5}, 18),
-                Arguments.of(15, new int[]{2, 3, 7}, 6),
-                Arguments.of(16, new int[]{3, 4, 7, 9}, 7),
-                Arguments.of(17, new int[]{1, 2, 3, 4}, 64),
-                Arguments.of(18, new int[]{5, 10, 12}, 2),
-                Arguments.of(19, new int[]{1, 5, 10}, 22),
-                Arguments.of(20, new int[]{2, 3, 7, 10}, 13)
+                Arguments.of(14, new int[]{1, 2, 5}, 16),
+                Arguments.of(15, new int[]{2, 3, 7}, 5),
+                Arguments.of(16, new int[]{3, 4, 7, 9}, 5),
+                Arguments.of(17, new int[]{1, 2, 3, 4}, 72),
+                Arguments.of(18, new int[]{5, 10, 12}, 0),
+                Arguments.of(19, new int[]{1, 5, 10}, 6),
+                Arguments.of(20, new int[]{2, 3, 7, 10}, 12)
         );
     }
 
