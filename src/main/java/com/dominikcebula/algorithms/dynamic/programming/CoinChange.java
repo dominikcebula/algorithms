703b65ba7f0b9972c7f09e4312957e01d854a20e
Dominik Cebula <dominikcebula@gmail.com>
2024-08-08 20:35:03 +0200

    Dynamic Programming Algorithms - Coin Change

--- src/main/java/com/dominikcebula/algorithms/dynamic/programming/CoinChange.java
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/CoinChange.java
@@ -2,6 +2,20 @@ package com.dominikcebula.algorithms.dynamic.programming;
 
 public class CoinChange {
     public int solve(int sum, int[] coinDenominations) {
-        return 0;
+        return solve(sum, coinDenominations, 0);
+    }
+
+    private int solve(int sum, int[] coinDenominations, int currentCoin) {
+        if (sum == 0)
+            return 1;
+
+        if (sum < 0)
+            return 0;
+
+        if (currentCoin >= coinDenominations.length)
+            return 0;
+
+        return solve(sum - coinDenominations[currentCoin], coinDenominations, currentCoin)
+                + solve(sum, coinDenominations, currentCoin + 1);
     }
 }
