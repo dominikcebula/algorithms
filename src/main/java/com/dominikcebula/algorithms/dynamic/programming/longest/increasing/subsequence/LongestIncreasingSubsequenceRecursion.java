44985130780455cca5d2c0a77e6de1824dd14222
Dominik Cebula <dominikcebula@gmail.com>
2024-08-10 15:55:42 +0200

    Dynamic Programming Algorithms - Longest Increasing Subsequence

--- src/main/java/com/dominikcebula/algorithms/dynamic/programming/longest/increasing/subsequence/LongestIncreasingSubsequenceRecursion.java
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/longest/increasing/subsequence/LongestIncreasingSubsequenceRecursion.java
@@ -2,6 +2,41 @@ package com.dominikcebula.algorithms.dynamic.programming.longest.increasing.subs
 
 public class LongestIncreasingSubsequenceRecursion {
     public int findLongestIncreasingSubsequence(int[] elements) {
-        return 0;
+        if (elements.length == 0)
+            return 0;
+
+        if (elements.length == 1)
+            return 1;
+
+        int longestIncreasingSubsequence = 0;
+
+        for (int startIdx = 0; startIdx < elements.length; startIdx++) {
+            longestIncreasingSubsequence = Math.max(
+                    longestIncreasingSubsequence,
+                    findLongestIncreasingSubsequence(elements, elements[startIdx], startIdx) + 1
+            );
+        }
+
+        return longestIncreasingSubsequence;
+    }
+
+    private int findLongestIncreasingSubsequence(int[] elements, int startElementValue, int currentElementIdx) {
+        if (currentElementIdx == elements.length)
+            return 1;
+
+        int longestIncreasingSubsequence = 0;
+
+        for (int candidateIdx = currentElementIdx + 1; candidateIdx < elements.length; candidateIdx++) {
+            int candidateValue = elements[candidateIdx];
+
+            if (candidateValue > startElementValue) {
+                longestIncreasingSubsequence = Math.max(
+                        longestIncreasingSubsequence,
+                        findLongestIncreasingSubsequence(elements, elements[candidateIdx], candidateIdx) + 1
+                );
+            }
+        }
+
+        return longestIncreasingSubsequence;
     }
 }
