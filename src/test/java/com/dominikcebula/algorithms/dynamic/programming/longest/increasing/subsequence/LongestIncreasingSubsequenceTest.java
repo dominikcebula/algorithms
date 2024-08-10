71f295d7f7289a91af7fe781843006543c01c3a4
Dominik Cebula <dominikcebula@gmail.com>
2024-08-10 15:58:21 +0200

    Dynamic Programming Algorithms - Longest Increasing Subsequence

--- src/test/java/com/dominikcebula/algorithms/dynamic/programming/longest/increasing/subsequence/LongestIncreasingSubsequenceTest.java
+++ src/test/java/com/dominikcebula/algorithms/dynamic/programming/longest/increasing/subsequence/LongestIncreasingSubsequenceTest.java
@@ -15,12 +15,20 @@ class LongestIncreasingSubsequenceTest {
         return Stream.of(
                 Arguments.of(new int[]{}, 0),
                 Arguments.of(new int[]{10}, 1),
+                Arguments.of(new int[]{10, 20, 30}, 3),
+                Arguments.of(new int[]{30, 20, 10}, 1),
                 Arguments.of(new int[]{3, 10, 2, 1, 20}, 3),
                 Arguments.of(new int[]{50, 3, 10, 7, 40, 80}, 4),
-                Arguments.of(new int[]{30, 20, 10}, 1),
                 Arguments.of(new int[]{50, 40, 30, 20, 10}, 1),
                 Arguments.of(new int[]{10, 20, 35, 80}, 4),
-                Arguments.of(new int[]{10, 20, 30, 40, 50}, 5)
+                Arguments.of(new int[]{10, 20, 30, 40, 50}, 5),
+                Arguments.of(new int[]{5, 7, 4, 8, 2, 9, 10}, 5),
+                Arguments.of(new int[]{2, 2, 2, 2, 2}, 1),
+                Arguments.of(new int[]{3, 4, -1, 0, 6, 2, 3}, 4),
+                Arguments.of(new int[]{1, 3, 6, 7, 9, 4, 10, 5, 6}, 6),
+                Arguments.of(new int[]{10, 9, 2, 5, 3, 7, 101, 18}, 4),
+                Arguments.of(new int[]{1, 2, 3, 2, 3, 4, 5}, 5),
+                Arguments.of(new int[]{9, 1, 3, 7, 5, 6, 20, 15}, 5)
         );
     }
 
