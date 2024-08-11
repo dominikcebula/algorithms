325c545f348cc2a458b673e138dc247a595f5ab9
Dominik Cebula <dominikcebula@gmail.com>
2024-08-11 20:07:20 +0200

    Dynamic Programming Algorithms - Longest Common Subsequence

--- src/main/java/com/dominikcebula/algorithms/dynamic/programming/longest/common/subsequence/LongestCommonSubsequenceRecursion.java
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/longest/common/subsequence/LongestCommonSubsequenceRecursion.java
@@ -1,7 +1,16 @@
 package com.dominikcebula.algorithms.dynamic.programming.longest.common.subsequence;
 
 public class LongestCommonSubsequenceRecursion {
-    int findLongestCommonSubsequence(String string1, String string2) {
-        return 0;
+    public int findLongestCommonSubsequence(String string1, String string2) {
+        return findLongestCommonSubsequence(string1, 0, string2, 0);
+    }
+
+    private int findLongestCommonSubsequence(String string1, int string1Idx, String string2, int string2Idx) {
+        if (string1Idx == string1.length() || string2Idx == string2.length()) return 0;
+
+        if (string1.charAt(string1Idx) == string2.charAt(string2Idx))
+            return findLongestCommonSubsequence(string1, string1Idx + 1, string2, string2Idx + 1) + 1;
+        else
+            return Math.max(findLongestCommonSubsequence(string1, string1Idx, string2, string2Idx + 1), findLongestCommonSubsequence(string1, string1Idx + 1, string2, string2Idx));
     }
 }
