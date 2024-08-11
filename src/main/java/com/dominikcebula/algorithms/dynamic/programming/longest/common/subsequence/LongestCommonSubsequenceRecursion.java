4b0e37a5791dc383a20522f064a1ca09e9157e5f
Dominik Cebula <dominikcebula@gmail.com>
2024-08-11 19:43:36 +0200

    Dynamic Programming Algorithms - Longest Common Subsequence

---
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/longest/common/subsequence/LongestCommonSubsequenceRecursion.java
@@ -0,0 +1,7 @@
+package com.dominikcebula.algorithms.dynamic.programming.longest.common.subsequence;
+
+public class LongestCommonSubsequenceRecursion {
+    int findLongestCommonSubsequence(String string1, String string2) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/dominikcebula/algorithms/dynamic/programming/longest/common/subsequence/LongestCommonSubsequenceTest.java
@@ -0,0 +1,30 @@
+package com.dominikcebula.algorithms.dynamic.programming.longest.common.subsequence;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LongestCommonSubsequenceTest {
+    private final LongestCommonSubsequenceRecursion longestCommonSubsequenceRecursion = new LongestCommonSubsequenceRecursion();
+
+    private static Stream<Arguments> longestCommonSubsequenceTestCases() {
+        return Stream.of(
+                Arguments.of("ABC", "ACD", 2),
+                Arguments.of("ABC", "CBA", 1),
+                Arguments.of("XYZW", "XYWZ", 3),
+                Arguments.of("AGGTAB", "GXTXAYB", 4)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("longestCommonSubsequenceTestCases")
+    void shouldFindLongestCommonSubsequenceUsingRecursion(String string1, String string2, int expectedLongestCommonSubsequence) {
+        int longestCommonSubsequence = longestCommonSubsequenceRecursion.findLongestCommonSubsequence(string1, string2);
+
+        assertEquals(expectedLongestCommonSubsequence, longestCommonSubsequence);
+    }
+}
