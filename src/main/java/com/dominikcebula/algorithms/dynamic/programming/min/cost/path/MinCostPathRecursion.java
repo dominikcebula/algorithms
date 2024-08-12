8df33293048d9566822758649d08b2c89f536b93
Dominik Cebula <dominikcebula@gmail.com>
2024-08-12 20:03:39 +0200

    Dynamic Programming Algorithms - Min Cost Path

--- src/main/java/com/dominikcebula/algorithms/dynamic/programming/min/cost/path/MinCostPathRecursion.java
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/min/cost/path/MinCostPathRecursion.java
@@ -1,7 +1,25 @@
 package com.dominikcebula.algorithms.dynamic.programming.min.cost.path;
 
+import java.util.stream.IntStream;
+
 public class MinCostPathRecursion {
+    private static final int NO_VALUE = -1;
+
     public int findMinCostPath(int[][] costs, int targetPositionM, int targetPositionN) {
-        return 0;
+        if (targetPositionM < 0 || targetPositionN < 0)
+            return NO_VALUE;
+
+        return costs[targetPositionM][targetPositionN] + min(
+                findMinCostPath(costs, targetPositionM - 1, targetPositionN - 1),
+                findMinCostPath(costs, targetPositionM, targetPositionN - 1),
+                findMinCostPath(costs, targetPositionM - 1, targetPositionN)
+        );
+    }
+
+    private static int min(int a, int b, int c) {
+        return IntStream.of(a, b, c)
+                .filter(cost -> cost != NO_VALUE)
+                .min()
+                .orElse(0);
     }
 }
