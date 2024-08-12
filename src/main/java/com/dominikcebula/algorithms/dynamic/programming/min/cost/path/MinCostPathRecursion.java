630ec00b748f2813a68572b69643aab13b904edf
Dominik Cebula <dominikcebula@gmail.com>
2024-08-12 19:50:09 +0200

    Dynamic Programming Algorithms - Min Cost Path

---
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/min/cost/path/MinCostPathRecursion.java
@@ -0,0 +1,7 @@
+package com.dominikcebula.algorithms.dynamic.programming.min.cost.path;
+
+public class MinCostPathRecursion {
+    public int findMinCostPath(int[][] costs, int targetPositionM, int targetPositionN) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/dominikcebula/algorithms/dynamic/programming/min/cost/path/MinCostPathRecursionTest.java
@@ -0,0 +1,31 @@
+package com.dominikcebula.algorithms.dynamic.programming.min.cost.path;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MinCostPathRecursionTest {
+    private final MinCostPathRecursion minCostPathRecursion = new MinCostPathRecursion();
+
+    private static Stream<Arguments> minCostPathTestCases() {
+        return Stream.of(
+                Arguments.of(new int[][]{
+                                {1, 2, 3},
+                                {4, 8, 2},
+                                {1, 5, 3}},
+                        2, 2, 8)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("minCostPathTestCases")
+    void shouldFindMinCostPathUsingRecursion(int[][] costs, int targetPositionM, int targetPositionN, int expectedMinCost) {
+        int minCostPath = minCostPathRecursion.findMinCostPath(costs, targetPositionM, targetPositionN);
+
+        assertEquals(expectedMinCost, minCostPath);
+    }
+}
