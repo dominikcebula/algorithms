9cd723384588051ac0ea68e4cf20ded3919852a3
Dominik Cebula <dominikcebula@gmail.com>
2024-08-13 21:27:30 +0200

    Dynamic Programming Algorithms - Knapsack Problem

--- src/test/java/com/dominikcebula/algorithms/dynamic/programming/knapsack/problem/KnapsackProblemTest.java
+++ src/test/java/com/dominikcebula/algorithms/dynamic/programming/knapsack/problem/KnapsackProblemTest.java
@@ -24,6 +24,66 @@ class KnapsackProblemTest {
                         new int[]{4, 5, 6},
                         3, 3,
                         0
+                ),
+                Arguments.of(
+                        new int[]{20, 5, 10, 40, 15, 25},
+                        new int[]{1, 2, 3, 8, 7, 4},
+                        6, 10,
+                        60
+                ),
+                Arguments.of(
+                        new int[]{24, 18, 18, 10},
+                        new int[]{24, 10, 10, 7},
+                        4, 25,
+                        36
+                ),
+                Arguments.of(
+                        new int[]{10, 20, 30},
+                        new int[]{1, 1, 1},
+                        3, 2,
+                        50
+                ),
+                Arguments.of(
+                        new int[]{5, 7, 12, 18, 3},
+                        new int[]{1, 3, 4, 5, 1},
+                        5, 7,
+                        26
+                ),
+                Arguments.of(
+                        new int[]{15, 14, 10, 45, 30},
+                        new int[]{2, 5, 1, 3, 4},
+                        5, 7,
+                        75
+                ),
+                Arguments.of(
+                        new int[]{10, 20, 15, 25},
+                        new int[]{1, 3, 4, 2},
+                        4, 5,
+                        45
+                ),
+                Arguments.of(
+                        new int[]{20, 30, 50, 10},
+                        new int[]{3, 4, 5, 2},
+                        4, 8,
+                        70
+                ),
+                Arguments.of(
+                        new int[]{7, 2, 9, 4},
+                        new int[]{3, 1, 2, 1},
+                        4, 5,
+                        16
+                ),
+                Arguments.of(
+                        new int[]{16, 19, 23, 28, 40},
+                        new int[]{2, 3, 4, 5, 6},
+                        5, 10,
+                        63
+                ),
+                Arguments.of(
+                        new int[]{50, 40, 30, 20},
+                        new int[]{5, 4, 6, 3},
+                        4, 10,
+                        90
                 )
         );
     }
