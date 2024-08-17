2b26771204b3139ebeba1e6eadbd22f849e52741
Dominik Cebula <dominikcebula@gmail.com>
2024-08-17 15:11:47 +0200

    Dynamic Programming Algorithms - Palindrome Partitioning

--- src/main/java/com/dominikcebula/algorithms/dynamic/programming/palindrome/partitioning/PalindromePartitioningTabulation.java
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/palindrome/partitioning/PalindromePartitioningTabulation.java
@@ -7,37 +7,50 @@ public class PalindromePartitioningTabulation {
 
     private int findMinCutsToGetPalindromes(char[] string) {
         int[][] minCuts = new int[string.length][string.length];
+        boolean[][] palindromes = new boolean[string.length][string.length];
 
-        for (int candidateLength = 2; candidateLength <= string.length; candidateLength++) {
-            for (int beginIdx = 0; beginIdx + candidateLength - 1 < string.length; beginIdx++) {
-                int endIdx = beginIdx + candidateLength - 1;
-
-                if (isPalindrome(string, beginIdx, endIdx)) {
-                    minCuts[beginIdx][endIdx] = 0;
-                } else {
-                    minCuts[beginIdx][endIdx] = Integer.MAX_VALUE;
-                    for (int cutIdx = beginIdx; cutIdx < endIdx; cutIdx++) {
-                        minCuts[beginIdx][endIdx] = Math.min(
-                                minCuts[beginIdx][endIdx],
-                                1 + minCuts[beginIdx][cutIdx] + minCuts[cutIdx + 1][endIdx]
-                        );
-                    }
-                }
-            }
-        }
+        initializeSingleCharPalindromes(string, minCuts, palindromes);
+        calculateCuts(string, minCuts, palindromes);
 
         return minCuts[0][string.length - 1];
     }
 
-    private boolean isPalindrome(char[] string, int beginIdx, int endIdx) {
-        while (beginIdx < endIdx) {
-            if (string[beginIdx] != string[endIdx])
-                return false;
-
-            ++beginIdx;
-            --endIdx;
+    private void initializeSingleCharPalindromes(char[] string, int[][] minCuts, boolean[][] palindromes) {
+        for (int sameCharIndex = 0; sameCharIndex < string.length; sameCharIndex++) {
+            minCuts[sameCharIndex][sameCharIndex] = 0;
+            palindromes[sameCharIndex][sameCharIndex] = true;
         }
+    }
 
-        return true;
+    private void calculateCuts(char[] string, int[][] minCuts, boolean[][] palindromes) {
+        for (int candidateLength = 2; candidateLength <= string.length; candidateLength++) {
+            for (int beginIdx = 0; beginIdx + candidateLength - 1 < string.length; beginIdx++) {
+                int endIdx = beginIdx + candidateLength - 1;
+
+                updatePalindromesForCandidate(string, palindromes, candidateLength, beginIdx, endIdx);
+                updateCutsForCandidate(minCuts, palindromes, beginIdx, endIdx);
+            }
+        }
+    }
+
+    private void updatePalindromesForCandidate(char[] string, boolean[][] palindromes, int candidateLength, int beginIdx, int endIdx) {
+        if (candidateLength == 2)
+            palindromes[beginIdx][endIdx] = string[beginIdx] == string[endIdx];
+        else
+            palindromes[beginIdx][endIdx] = string[beginIdx] == string[endIdx] && palindromes[beginIdx + 1][endIdx - 1];
+    }
+
+    private void updateCutsForCandidate(int[][] minCuts, boolean[][] palindromes, int beginIdx, int endIdx) {
+        if (palindromes[beginIdx][endIdx]) {
+            minCuts[beginIdx][endIdx] = 0;
+        } else {
+            minCuts[beginIdx][endIdx] = Integer.MAX_VALUE;
+            for (int cutIdx = beginIdx; cutIdx < endIdx; cutIdx++) {
+                minCuts[beginIdx][endIdx] = Math.min(
+                        minCuts[beginIdx][endIdx],
+                        1 + minCuts[beginIdx][cutIdx] + minCuts[cutIdx + 1][endIdx]
+                );
+            }
+        }
     }
 }
