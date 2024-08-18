ff1cbfb4a218403162a3fc3154f6deed74fccda1
Dominik Cebula <dominikcebula@gmail.com>
2024-08-18 15:57:17 +0200

    Data Structures - Stack

--- src/test/java/com/dominikcebula/data/structures/stack/StackTest.java
+++ src/test/java/com/dominikcebula/data/structures/stack/StackTest.java
@@ -12,4 +12,71 @@ class StackTest {
         assertThat(stack.isEmpty())
                 .isTrue();
     }
+
+    @Test
+    void shouldAddOneElement() {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+
+        assertThat(stack.peek())
+                .isEqualTo(1);
+        assertThat(stack.isEmpty())
+                .isFalse();
+    }
+
+    @Test
+    void shouldAddThreeElements() {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertThat(stack.peek())
+                .isEqualTo(3);
+        assertThat(stack.isEmpty())
+                .isFalse();
+    }
+
+    @Test
+    void shouldAddAndPopElements() {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertThat(stack.pop()).isEqualTo(3);
+        assertThat(stack.pop()).isEqualTo(2);
+
+        assertThat(stack.peek())
+                .isEqualTo(1);
+        assertThat(stack.isEmpty())
+                .isFalse();
+    }
+
+    @Test
+    void shouldAddAndPopAllElements() {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        assertThat(stack.pop()).isEqualTo(3);
+        assertThat(stack.pop()).isEqualTo(2);
+
+        stack.push(4);
+        stack.push(5);
+
+        assertThat(stack.pop()).isEqualTo(5);
+        assertThat(stack.pop()).isEqualTo(4);
+        assertThat(stack.pop()).isEqualTo(1);
+
+        assertThat(stack.peek())
+                .isNull();
+        assertThat(stack.isEmpty())
+                .isTrue();
+    }
 }
