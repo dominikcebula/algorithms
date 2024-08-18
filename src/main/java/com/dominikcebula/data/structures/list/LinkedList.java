603160e9d4624f715255af02168577c487ea9c20
Dominik Cebula <dominikcebula@gmail.com>
2024-08-18 15:07:26 +0200

    Data Structures - LinkedList

--- src/main/java/com/dominikcebula/data/structures/list/LinkedList.java
+++ src/main/java/com/dominikcebula/data/structures/list/LinkedList.java
@@ -1,12 +1,28 @@
 package com.dominikcebula.data.structures.list;
 
+import java.lang.reflect.Array;
+
 public class LinkedList<T> {
 
+    private final Class<T> elementClass;
     private Node<T> head;
     private Node<T> tail;
+    private int size;
+
+    public LinkedList(Class<T> elementClass) {
+        this.elementClass = elementClass;
+    }
 
     public void addFirst(T element) {
+        if (head == null) {
+            head = tail = new Node<>(element);
+        } else {
+            Node<T> newNode = new Node<>(element);
+            newNode.next = head;
+            head = newNode;
+        }
 
+        ++size;
     }
 
     public void addLast(T element) {
@@ -30,13 +46,50 @@ public class LinkedList<T> {
     }
 
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public T[] toArray() {
-        return null;
+        T[] array = (T[]) Array.newInstance(elementClass, size);
+
+        Node<T> currentNode = head;
+        int currentIndex = 0;
+
+        while (currentNode != null) {
+            array[currentIndex++] = currentNode.getElement();
+            currentNode = currentNode.getNext();
+        }
+
+        return array;
     }
 
-    private record Node<T>(T element, Node<T> prev, Node<T> next) {
+    private static final class Node<T> {
+        private final T element;
+        private Node<T> prev;
+        private Node<T> next;
+
+        Node(T element) {
+            this.element = element;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public Node<T> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
     }
 }
--- src/test/java/com/dominikcebula/data/structures/list/LinkedListTest.java
+++ src/test/java/com/dominikcebula/data/structures/list/LinkedListTest.java
@@ -1,5 +1,39 @@
 package com.dominikcebula.data.structures.list;
 
-class LinkedListTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LinkedListTest {
+    @Test
+    void shouldBeEmptyByDefault() {
+        LinkedList<Integer> list = new LinkedList<>(Integer.class);
+
+        assertThat(list.isEmpty())
+                .isTrue();
+        assertThat(list.toArray())
+                .isEmpty();
+    }
+
+    @Test
+    void shouldAddOneElementToHead() {
+        LinkedList<Integer> list = new LinkedList<>(Integer.class);
+
+        list.addFirst(1);
+
+        assertThat(list.toArray())
+                .isEqualTo(new Integer[]{1});
+    }
+
+    @Test
+    void shouldAddThreeElementsToHead() {
+        LinkedList<Integer> list = new LinkedList<>(Integer.class);
+
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+
+        assertThat(list.toArray())
+                .isEqualTo(new Integer[]{3, 2, 1});
+    }
 }
