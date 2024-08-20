cab4e0f05104436d395207dc8d4ed05a394922ff
Dominik Cebula <dominikcebula@gmail.com>
2024-08-20 18:55:18 +0200

    Data Structures - Binary Search Tree

--- src/main/java/com/dominikcebula/data/structures/tree/BinarySearchTree.java
+++ src/main/java/com/dominikcebula/data/structures/tree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package com.dominikcebula.data.structures.tree;
 
+import com.dominikcebula.data.structures.list.LinkedList;
+
 import java.lang.reflect.Array;
 
 public class BinarySearchTree<T extends Comparable<T>> {
@@ -13,8 +15,16 @@ public class BinarySearchTree<T extends Comparable<T>> {
     }
 
     public void add(T value) {
-        if (root == null)
+        if (root == null) {
             root = new Node<>(value);
+        } else {
+            Node<T> parentNodeToInsertValue = findParentNodeToInsertValue(value);
+
+            if (value.compareTo(parentNodeToInsertValue.getValue()) < 0)
+                parentNodeToInsertValue.setLeft(new Node<>(value));
+            else
+                parentNodeToInsertValue.setRight(new Node<>(value));
+        }
 
         ++size;
     }
@@ -22,37 +32,78 @@ public class BinarySearchTree<T extends Comparable<T>> {
     public T[] toArray() {
         T[] array = (T[]) Array.newInstance(elementClass, size);
 
-        array[0] = root.getValue();
+        if (size == 0)
+            return array;
+
+        int arrayIndex = 0;
+
+        LinkedList<Node<T>> nodesStack = new LinkedList<>((Class<Node<T>>) root.getClass());
+        nodesStack.addLast(root);
+
+        while (!nodesStack.isEmpty()) {
+            Node<T> currentNode = nodesStack.getFirst();
+            nodesStack.removeFirst();
+
+            array[arrayIndex++] = currentNode.getValue();
+
+            if (currentNode.hasLeft())
+                nodesStack.addLast(currentNode.getLeft());
+            if (currentNode.hasRight())
+                nodesStack.addLast(currentNode.getRight());
+        }
 
         return array;
     }
 
-    private static class Node<T> {
+    private Node<T> findParentNodeToInsertValue(T value) {
+        Node<T> currentNode = root;
+        Node<T> parentNode = currentNode;
+
+        while (currentNode != null) {
+            parentNode = currentNode;
+            if (value.compareTo(currentNode.getValue()) < 0)
+                currentNode = currentNode.getLeft();
+            else
+                currentNode = currentNode.getRight();
+        }
+
+        return parentNode;
+    }
+
+    private static class Node<T extends Comparable<T>> {
         private final T value;
         private Node<T> left;
         private Node<T> right;
 
-        public Node(T value) {
+        Node(T value) {
             this.value = value;
         }
 
-        public T getValue() {
+        T getValue() {
             return value;
         }
 
-        public Node<T> getLeft() {
+        boolean hasLeft() {
+            return left != null;
+        }
+
+        Node<T> getLeft() {
             return left;
         }
 
-        public void setLeft(Node<T> left) {
+        void setLeft(Node<T> left) {
             this.left = left;
         }
 
-        public Node<T> getRight() {
+        boolean hasRight() {
+            return right != null;
+        }
+
+        Node<T> getRight() {
             return right;
         }
 
-        public void setRight(Node<T> right) {
+        void setRight(Node<T> right) {
             this.right = right;
         }
     }
--- src/test/java/com/dominikcebula/data/structures/tree/BinarySearchTreeTest.java
+++ src/test/java/com/dominikcebula/data/structures/tree/BinarySearchTreeTest.java
@@ -14,4 +14,20 @@ class BinarySearchTreeTest {
         assertThat(tree.toArray())
                 .isEqualTo(new Integer[]{50});
     }
+
+    @Test
+    void shouldAddMultipleElements() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>(Integer.class);
+
+        tree.add(50);
+        tree.add(30);
+        tree.add(70);
+        tree.add(20);
+        tree.add(40);
+        tree.add(60);
+        tree.add(80);
+
+        assertThat(tree.toArray())
+                .isEqualTo(new Integer[]{50, 30, 70, 20, 40, 60, 80});
+    }
 }
