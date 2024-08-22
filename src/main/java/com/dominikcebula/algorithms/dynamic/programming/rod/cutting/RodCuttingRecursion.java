bec7997c632fa828599bc1b64d6764b708f0df23
Dominik Cebula <dominikcebula@gmail.com>
2024-08-22 14:59:48 +0200

    Dynamic Programming Algorithms - Rod Cutting

--- src/main/java/com/dominikcebula/algorithms/dynamic/programming/rod/cutting/RodCuttingRecursion.java
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/rod/cutting/RodCuttingRecursion.java
@@ -2,6 +2,27 @@ package com.dominikcebula.algorithms.dynamic.programming.rod.cutting;
 
 public class RodCuttingRecursion {
     public int findMaxCuttingRodValue(int[] prices) {
-        return 0;
+        int rodLength = prices.length;
+
+        return findMaxCuttingRodValue(prices, prices.length - 1, rodLength);
+    }
+
+    private int findMaxCuttingRodValue(int[] prices, int currentIndex, int rodLength) {
+        int currentPieceLength = currentIndex + 1;
+
+        if (currentIndex < 0)
+            return 0;
+
+        if (currentPieceLength == 1) {
+            return rodLength * prices[currentIndex];
+        }
+
+        if (currentPieceLength > rodLength)
+            return findMaxCuttingRodValue(prices, currentIndex - 1, rodLength);
+        else
+            return Math.max(
+                    prices[currentIndex] + findMaxCuttingRodValue(prices, currentIndex - 1, rodLength - currentPieceLength),
+                    findMaxCuttingRodValue(prices, currentIndex - 1, rodLength)
+            );
     }
 }
