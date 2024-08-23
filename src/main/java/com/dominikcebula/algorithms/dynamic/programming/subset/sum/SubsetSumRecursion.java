3352028f8253d327f0cbbb14dbd2d2c77dea71d0
Dominik Cebula <dominikcebula@gmail.com>
2024-08-23 09:27:35 +0200

    Dynamic Programming Algorithms - Subset Sum

--- src/main/java/com/dominikcebula/algorithms/dynamic/programming/subset/sum/SubsetSumRecursion.java
+++ src/main/java/com/dominikcebula/algorithms/dynamic/programming/subset/sum/SubsetSumRecursion.java
@@ -1,7 +1,20 @@
 package com.dominikcebula.algorithms.dynamic.programming.subset.sum;
 
 public class SubsetSumRecursion {
-    boolean doesSubsetSumExist(int[] elements, int sum) {
-        return false;
+    public boolean doesSubsetSumExist(int[] elements, int sum) {
+        return doesSubsetSumExist(elements, sum, elements.length - 1);
+    }
+
+    private boolean doesSubsetSumExist(int[] elements, int sum, int currentIndex) {
+        if (sum == 0)
+            return true;
+
+        if (sum < 0 || currentIndex < 0)
+            return false;
+
+        int currentElementValue = elements[currentIndex];
+
+        return doesSubsetSumExist(elements, sum - currentElementValue, currentIndex - 1)
+                || doesSubsetSumExist(elements, sum, currentIndex - 1);
     }
 }
